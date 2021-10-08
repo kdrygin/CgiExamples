7fd13b743884b4b9943c1133dfb3e9e47d87f148
Дрыгин Константин <kdrygin@gmail.com>
2021-10-08 13:40:19 -0700

    HTML form handle post  method example added

---
+++ HtmlFormHandlePost/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HtmlFormHandlePost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // get post data length
+            var dataLen = int.Parse(Environment.GetEnvironmentVariable("CONTENT_LENGTH"));
+
+            //get post data
+            var rowPostData = new char[dataLen + 1];
+            for(int i = 0; i<dataLen; ++i)
+            {
+                rowPostData[i] = (char)Console.Read();
+            }
+
+            // get form fields
+            var fields = new String(rowPostData).Split("&");
+
+            // show form data
+            Console.WriteLine("Content-Type: text/html \n\n");
+            Console.WriteLine("<html><head><title> Current Time</title></head>");
+            Console.WriteLine("<body>");
+
+            Console.WriteLine($"<p>{dataLen}</p>");
+
+            foreach (var field in fields)
+            {
+                Console.WriteLine($"<p>{field}</p>");
+            }
+
+            Console.WriteLine("</body></html>");
+        }
+    }
+}
