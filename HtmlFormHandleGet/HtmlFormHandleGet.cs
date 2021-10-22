140875747156c991bff920189cb5b48ffb44c3d1
Konstantin Drygin <kdrygin@gmail.com>
2021-10-22 23:19:21 +0300

    new project added

---
+++ HtmlFormHandleGet/HtmlFormHandleGet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HtmlFormHandleGet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Content-Type: text/html \n\n");
+            var queryStr = Environment.GetEnvironmentVariable("QUERY_STRING");
+            var requestMethod = Environment.GetEnvironmentVariable("REQUEST_METHOD");
+            Console.WriteLine($"{queryStr} {requestMethod}");
+        }
+    }
+}
